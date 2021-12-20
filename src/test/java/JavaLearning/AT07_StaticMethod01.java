f6d509f5b732520f88559fbbd6cd0f1624b6b93b
Gogula16 <96359558+Gogula16@users.noreply.github.com>
2021-12-20 12:37:13 +1100

    Version1B.20.12.2021.12:25

--- src/test/java/JavaLearning/AT07_StaticMethod01.java
+++ src/test/java/JavaLearning/AT07_StaticMethod01.java
@@ -3,7 +3,7 @@ package JavaLearning;
 import org.junit.Test;
 
 public class AT07_StaticMethod01 {
-    //we are declaring static here, so we can call this method by another class without creating object
+    //we are declaring static here, so we can call this method by another class without creating object/////
     @Test
        public static void Test10(){
 
